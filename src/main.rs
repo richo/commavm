ad9bceea4bdccd92d9179428d2683a4179d01383
Richo Healey <richo@psych0tik.net>
2015-03-08 22:14:44 -0700

    Really goofy harness that'll transform func names

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,37 @@ extern crate syntax;
 
 use syntax::{parse,ast,abi};
 use std::path;
+use std::old_io::stdio;
+
+struct Func {
+    pub name: String,
+}
+
+struct Ctx {
+    fns: Vec<Func>,
+}
+
+impl Ctx {
+    pub fn new() -> Ctx {
+        Ctx { fns: vec![] }
+    }
+
+
+    pub fn fns(&self) -> &Vec<Func>{
+        &self.fns
+    }
+
+    pub fn add_fn(&mut self, f: Func) {
+        self.fns.push(f);
+    }
+}
+
+fn emit(ctx: Ctx) {
+    for f in ctx.fns() {
+        print!("{}()", f.name);
+        stdio::println("{\n}");
+    }
+}
 
 fn main() {
     let ref input = ::std::os::args()[1];
@@ -16,5 +47,18 @@ fn main() {
     let mut parser = parse::new_parser_from_file(&sess, cfg, &file);
 
     let krate = parser.parse_crate_mod();
-    println!("{:?}", krate);
+
+    let mut ctx = Ctx::new();
+
+    for it in &krate.module.items {
+        match it.node {
+            ast::ItemFn(ref dec, safety, abi, _, ref blk) => {
+                ctx.add_fn(Func { name: it.ident.as_str().to_string() });
+            },
+            _ => {},
+        }
+    }
+
+    // Jank
+    emit(ctx);
 }
